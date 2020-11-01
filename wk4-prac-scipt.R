7df4af0dc5316bc6012b660adb4b9011b1f9f2f1
HaochengSun722 <ucfnhsu@ucl.ac.uk>
2020-11-02 00:50:44 +0800

    Wk4_practical

---
+++ wk4-prac-scipt.R
@@ -0,0 +1,24 @@
+library(reprex)###reprex功能可以将你的代码复制给别人，以麻烦别人解决问题
+
+
+
+
+
+
+####WK4######
+
+A <- 1
+B <- 2
+C <- A+B
+C
+
+###default默认设置为GitHub读取格式
+reprex()
+#👇适用于stackoverflow
+reprex(venue="so")
+#👇适用于slack
+reprex(venue="r")
+
+x <- 1+1
+y <- 2+2
+
